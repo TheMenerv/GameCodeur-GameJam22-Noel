792be626454c80661aacdd518d3847ca047f7460
Menerv <mail@menerv.net>
2019-12-10 11:13:58 +0100

    Création d'une map vierge et calcule des coordonnées de la souris

--- main.lua
+++ main.lua
@@ -7,6 +7,11 @@ love.graphics.setDefaultFilter("nearest")
 
 local window = {}
 
+-- MAP
+local BLOC_SIZE = 32
+local map = {}
+local mouse = {}
+
 
 
 -- INITIALISATION
@@ -16,6 +21,16 @@ function love.load()
     window.width = love.graphics.getWidth()
     window.height = love.graphics.getHeight()
 
+    -- MAP
+    local l, c
+    map.c = {}
+    for l = 1, window.height / BLOC_SIZE do
+        map[l] = {}
+        for c = 1, window.width / BLOC_SIZE do
+            map[l][c] = 0
+        end
+    end
+
 end
 
 
@@ -24,7 +39,8 @@ end
 ---------
 function love.update(dt)
 
-    --
+    -- MAP
+    mouse = GetMousePos()
 
 end
 
@@ -34,7 +50,24 @@ end
 -------
 function love.draw()
 
-    --
+    -- MAP
+    love.graphics.push()
+    local l, c
+    for l = 1, #map do
+        for c = 1, #map[l] do
+            love.graphics.rectangle(
+                'fill',
+                (c * BLOC_SIZE) - BLOC_SIZE,
+                (l * BLOC_SIZE) - BLOC_SIZE,
+                BLOC_SIZE,
+                BLOC_SIZE
+            )
+        end
+    end
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("L="..tostring(mouse.l).." C="..tostring(mouse.c), 5, 5)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.pop()
 
 end
 
@@ -45,5 +78,22 @@ end
 function love.keypressed(key)
 
     print(key)
+
+end
+
+
+
+-- GET MOUSE POS
+----------------
+function GetMousePos()
+
+    local x, y = love.mouse.getPosition()
+
+    local myMouse = {}
+
+    myMouse.l = math.floor(y / BLOC_SIZE) + 1
+    myMouse.c = math.floor(x / BLOC_SIZE) + 1
+
+    return myMouse
 
 end

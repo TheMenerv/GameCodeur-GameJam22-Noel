a057e1ad5f7ed258dc424ae3bf7ba41f539d9799
Menerv <mail@menerv.net>
2019-12-10 11:12:42 +0100

    Commit initial

---
+++ main.lua
@@ -0,0 +1,49 @@
+---------------------
+-- Codé par Menerv --
+---------------------
+
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+
+local window = {}
+
+
+
+-- INITIALISATION
+-----------------
+function love.load()
+
+    window.width = love.graphics.getWidth()
+    window.height = love.graphics.getHeight()
+
+end
+
+
+
+-- UPDATE
+---------
+function love.update(dt)
+
+    --
+
+end
+
+
+
+-- DRAW
+-------
+function love.draw()
+
+    --
+
+end
+
+
+
+-- KEYBOARD
+-----------
+function love.keypressed(key)
+
+    print(key)
+
+end
